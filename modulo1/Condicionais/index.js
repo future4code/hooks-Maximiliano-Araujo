9f311bdf8387cc4568844d8ff2d484ce862fd545
Maximiliano <ranholimax@gmail.com>
2022-02-24 22:32:59 -0300

    ecercício de condicionais

---
+++ modulo1/Condicionais/index.js
@@ -0,0 +1,83 @@
+//   ==================================== exercício 01 =========================================
+// let idade = Number(prompt("Qual a sua idade? (Em números)"))
+
+// if (idade >= 18){
+//     console.log("Você pode dirigir")
+// } else {
+//     console.log("Você não pode dirigir")
+// }
+
+
+
+
+
+// ==================================== exercício 02 =========================================
+// let turnoDoDia = prompt ("Qual turno do dia vc estuda? M (matutino) V (vespertino) N (noturno)")
+
+// // if (turnoDoDia == "m") {
+// //     console.log("Bom dia")
+// // } else if  (turnoDoDia == "v") {
+// //     console.log("Boa tarde")
+// // } else if (turnoDoDia == "n") {
+// //     console.log("Boa noite")
+// // }
+
+
+
+
+// ==================================== exercício 03 =========================================
+// switch (turnoDoDia){
+//     case "m":
+//         console.log("bom dia")
+//         break
+//     case "v":
+//         console.log("Boa tarde")
+//         break
+//     case "n":
+//         console.log("Boa noite")            
+// }
+
+
+
+// ==================================== exercício 04 =========================================
+// let generoFilme = prompt ("Qual gênero de filme vocês vão assitir?")
+// let precoFilme = Number (prompt("Qual o valor do ingresso"))
+
+// function verFilme (generoEscolhido , valorDoFilme){
+//     if (
+//         generoEscolhido === "fantasia" &&
+//         valorDoFilme < 15
+//     ) {
+//     console.log("Bom filme")
+//     } else 
+//     console.log("Escolha outro filme :(")
+// } 
+
+
+// verFilme(generoFilme,precoFilme)
+
+
+
+// ==================================== Desafio 01 =========================================
+// let generoFilme = prompt ("Qual gênero de filme vocês vão assitir?")
+// let precoFilme = Number (prompt("Qual o valor do ingresso"))
+// let lanchinho = ["pipoca", "chocolate", "doces", "pastel"]
+
+// function verFilme (generoEscolhido , valorDoFilme){
+//     if (
+//         generoEscolhido == "fantasia" &&
+//         valorDoFilme < 15
+//     ) {
+//     let lanchinho = prompt ("Qual lanche você vai comer durante o filme?") 
+//     console.log("Bom filme")
+//     console.log(`Aproveite o seu ${lanchinho}`)
+//     } else 
+//     console.log("Escolha outro filme :(")
+// } 
+
+
+// verFilme(generoFilme,precoFilme)
+
+
+
+
