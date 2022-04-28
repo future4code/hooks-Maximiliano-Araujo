321b8d65cae279e28e556e63d80f634b7227ae55
MaxRanholi <73243210+MaxRanholi@users.noreply.github.com>
2022-04-28 14:48:35 -0300

    projeto-whatslab

--- modulo3/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,38 +1,19 @@
-import React from 'react';
-import './App.css';
-import { Container ,Buttons ,ButtonUser ,ButtonMessage,ButtonSend } from './styled';
+import React from "react";
+import {Container, Buttons, ButtonUser, ButtonMessage, ButtonSend} from "./styled"
 
 
+function App(){
+  return(
+    <Container>
+      
+      <Buttons></Buttons>
+      <ButtonUser>User</ButtonUser>      
+      <ButtonMessage>Enviar</ButtonMessage>
+      <ButtonSend>Send</ButtonSend>
 
+    </Container>
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <Container>
-        <Buttons>
-          <ButtonUser>User</ButtonUser>
-          <ButtonMessage>Message</ButtonMessage>
-          <ButtonSend>Send</ButtonSend>
-        </Buttons>
-      </Container>
-
-
-
-      /* <body classname="mainContainer">
-        <div className="container">
-          <div className="buttons">
-            <input className="buttonUser">User</input>
-            <input className="buttonMessage">Message</input>
-            <button className="buttonSend">Send</button>
-          </div>
-        </div>
-      </body> */
-    // </div>
-
-  );
+  )
 }
 
 export default App;
