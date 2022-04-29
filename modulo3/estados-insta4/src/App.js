67d8f1847bcb458e26c3de7ef747c046d23c2098
MaxRanholi <73243210+MaxRanholi@users.noreply.github.com>
2022-04-28 21:18:52 -0300

    mudancas

--- modulo3/estados-insta4/src/App.js
+++ modulo3/estados-insta4/src/App.js
@@ -9,28 +9,85 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+
+
+
+
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Márcio'}
-          fotoUsuario={'https://picsum.photos/49/49'}
-          fotoPost={'https://picsum.photos/200/149'}
-        />
-        <Post
-        nomeUsuario={'Mariana'}
-        fotoUsuario={'https://picsum.photos/60/60'}
-        fotoPost={'https://picsum.photos/200/160'}/>
-      </MainContainer>
-    );
+  state = {
+    post: [
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuario: "https://picsum.photos/50/50",
+        fotoPost: "https://picsum.photos/200/150",
+      },
+      {
+        nomeUsuario: "Márcio",
+        fotoUsuario: "https://picsum.photos/49/49",
+        fotoPost: "https://picsum.photos/200/149",
+      },
+      {
+        nomeUsuario: "Mariana",
+        fotoUsuario: "https://picsum.photos/60/60",
+        fotoPost: "https://picsum.photos/200/160",
+      }
+    ],
+
   }
+
+  render() {
+    const listaDeComponentes = this.state.pessoas.map((post) => {
+      return (
+        <p>
+          {post.nomeUsuario} - {post.fotoUsuario} - {post.fotoPost}
+        </p>
+      )
+    })
+    return (
+      {listaDeComponentes}
+    )
+  }
+
+  
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <MainContainer>
+//         <Post
+//           nomeUsuario={'Paulinha'}
+//           fotoUsuario={'https://picsum.photos/50/50'}
+//           fotoPost={'https://picsum.photos/200/150'}
+//         />
+//         <Post
+//           nomeUsuario={'Márcio'}
+//           fotoUsuario={'https://picsum.photos/49/49'}
+//           fotoPost={'https://picsum.photos/200/149'}
+//         />
+//         <Post
+//           nomeUsuario={'Mariana'}
+//           fotoUsuario={'https://picsum.photos/60/60'}
+//           fotoPost={'https://picsum.photos/200/160'} />
+//       </MainContainer>
+//     );
+//   }
+// }
 
 
 
