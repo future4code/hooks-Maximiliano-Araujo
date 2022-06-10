b55788e0da1ad6c69dd7df865b62e82c404c06f7
Max-Ranholi <ranholimax@gmail.com>
2022-06-10 18:18:59 -0300

    delete

--- modulo4/astromatch/astromatch/src/components/cardMatch/CardMatchStyled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components"
-
-export const CardMatchStyled = styled.div`
-display: flex;
-margin-left: 5px;
-gap: 10px;
-`
--- modulo4/astromatch/astromatch/src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/Max-Ranholi-Hooks"
-
-export const axiosConfig = {
-    headers: {
-        Authorization: "Max-Ranholi-Hooks"
-    }
-}
