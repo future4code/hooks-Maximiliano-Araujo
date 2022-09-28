6b058de1f38311fbe40c26750e2196a4101d66e2
Max-Ranholi <ranholimax@gmail.com>
2022-09-27 22:17:47 -0300

    feito

---
+++ modulo6/fluxo-dados-backend-exercicio/src/data.ts
@@ -0,0 +1,23 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number,
+}
+
+export const products: Product[] = [
+    {
+      id: "1",
+      name: "Garrafa Térmica 1L",
+      price: 35,
+    },
+    {
+      id: "2",
+      name: "Boné",
+      price: 15,
+    },
+    {
+      id: "3",
+      name: "Relógio",
+      price: 50,
+    },
+]
